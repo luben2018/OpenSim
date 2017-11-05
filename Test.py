eb5f1df9c763a3874a8271276fae87a78f290f69
Hernandez Vincent <vincent.hernandez1985@gmail.com>
2017-11-05 17:58:32 +0900

    Update

--- Test.py
+++ Test.py
@@ -1,8 +1,6 @@
 import opensim as osim
 
-dir_path = 'D:\Google Drive\Share to the world\GitHub\OpenSim'
-
-generic_MM_Path = dir_path + '\\FullBodyModel\\Rajagopal2015.osim'                 # Path to the Generic Musculoskeletal Model
+generic_MM_Path = 'FullBodyModel\\Rajagopal2015.osim'                 # Path to the Generic Musculoskeletal Model
 
 # Load the generic musculoskeletal model
 osimModel = osim.Model(generic_MM_Path)
